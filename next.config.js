3e0f04f69fa57e56855eb2ef0fa2626b1bd60965
satoterin <judekinsley911@gmail.com>
2022-01-25 05:56:06 +0800

    svg support added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  },
   exportPathMap: async function(
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
