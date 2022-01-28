eb0a480ed52d6f2c216775545d79ac487b31f73a
satoterin <judekinsley911@gmail.com>
2022-01-28 08:32:46 +0800

    language detector path fixed

--- components/languageSwitcher/index.js
+++ components/languageSwitcher/index.js
@@ -1,4 +1,4 @@
-import languageDetector from '../lib/languageDetector'
+import languageDetector from '../../lib/languageDetector'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
