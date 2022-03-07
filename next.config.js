2426ab5d9e9ed23aee2e190eb6f81f466472685e
satoterin <judekinsley911@gmail.com>
2022-03-07 18:18:28 +0800

    fix trailingSlash

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
     };
   },
 
+	trailingSlash: true,
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,7 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-import i18next from 'i18next';
 import LangHome from "./[lang]";
-import {findLanguage} from "../lib/lang";
-import {defaultLanguage} from "../i18n/config";
 
 export default function Home() {
-	const router = useRouter();
-
-	useEffect(() => {
-		const lang = findLanguage(i18next.languages)
-		const { pathname, asPath } = router;
-		if (pathname === '/' && lang !== defaultLanguage) {
-			console.log(router)
-			//router.push(`/${lang}${asPath}`);
-		}
-	}, []);
-
 	return <>
-		<LangHome language={defaultLanguage} />
+		<LangHome language='en' />
 	</>;
 }
