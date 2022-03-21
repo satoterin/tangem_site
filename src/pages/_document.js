a02c8989bedc2fe60b4421908a95f4f7a45bde82
satoterin <judekinsley911@gmail.com>
2022-03-21 16:49:47 +0800

    add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,21 +6,26 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <script
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=UA-115763532-5'
-        />
-       <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-115763532-5', { page_path: window.location.pathname });
+							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-N7NQVMC');
             `,
           }}
         />
       </Head>
       <body>
+        <noscript>
+	        <iframe
+		        src="https://www.googletagmanager.com/ns.html?id=GTM-N7NQVMC"
+						height="0"
+		        width="0"
+		        style={{ display: "none", visibility: "hidden"}}
+	        />
+				</noscript>
         <Main />
         <NextScript />
       </body>
