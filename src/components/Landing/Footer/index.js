40a5c9d5f8952f6996a91904125c77b38271924a
satoterin <judekinsley911@gmail.com>
2022-04-26 18:18:07 +0800

    JS-5 Add Swiss label

--- src/components/Landing/Footer/index.js
+++ src/components/Landing/Footer/index.js
@@ -4,7 +4,10 @@ import styles from './footer.module.scss'
 const Footer = () => {
 
   return (
-    <footer className={styles['footer']}>
+    <footer className={styles.footer}>
+	    <span className={styles.madeIn}>
+		    From Switzerland
+	    </span>
       <div className={styles['footer-copyright']}>
         Copyright © {(new Date()).getFullYear()} Tangem. All Rights Reserved.{"\n"}
         Global Headquarters, Tangem AG, Baarerstrasse 10, 6300 Zug, Switzerland
