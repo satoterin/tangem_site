48ab25e8b939084d49105f91ad4900490ec49fc6
satoterin <judekinsley911@gmail.com>
2022-06-09 22:24:39 +0800

    JS-28 Edit main page

--- src/pages/[lang]/partnership/index.js
+++ src/pages/[lang]/partnership/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import i18next from "i18next";
+import i18next, {t} from 'i18next';
 import Header from "../../../components/Common/Header";
 import React from "react";
 import SectionHero from "../../../components/Partnership/SectionHero";
@@ -20,17 +20,17 @@ const LangPartnership = () => {
 			<Head>
 				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<meta name="title" content={ i18next.t('title') } />
-				<meta name="description" content={ i18next.t('description') } />
+				<meta name="title" content={ t('pages.partnership.title') } />
+				<meta name="description" content={ t('description') } />
 				<meta property="og:type" content="website" />
 				<meta property="og:url" content="https://tangem.com" />
-				<meta property="og:site_name" content={ i18next.t('title') } />
-				<meta property="og:title" content={ i18next.t('title') } />
-				<meta property="og:description" content={ i18next.t('description') } />
+				<meta property="og:site_name" content={ t('title') } />
+				<meta property="og:title" content={ t('title') } />
+				<meta property="og:description" content={ t('description') } />
 				<meta property="og:image" content="https://tangem-staging.netlify.app/img/hero/hero-phone-1x.png" />
 				<meta property="og:video" content="https://www.youtube.com/watch?v=ST4jvcaE_UU" />
 				<meta property="og:locale" content="en_US" />
-				<title>Tangem Wallet — Hardware Wallet For Your Crypto</title>
+				<title>{t('pages.partnership.title')}</title>
 				<link rel='shortcut icon' href='/img/favicon/favicon.png' />
 				<link rel='apple-touch-icon' href='/img/favicon/favicon-180.png' />
 			</Head>
