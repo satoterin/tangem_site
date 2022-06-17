a2259ae9a566eb70ede039adeb985f49a7c28415
satoterin <judekinsley911@gmail.com>
2022-06-17 20:06:44 +0800

    JS-34 adaptation for IPad

---
+++ stylelint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  customSyntax: 'postcss-scss',
+  extends: ['stylelint-config-rational-order', 'stylelint-config-standard-scss'],
+  plugins: ['stylelint-a11y', 'stylelint-declaration-strict-value', 'stylelint-color-format'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'property-no-unknown': [true, { ignoreAtRules: ['supports'] }],
+
+    'scss/selector-no-redundant-nesting-selector': true,
+
+    'a11y/media-prefers-reduced-motion': true,
+    'a11y/no-outline-none': true,
+    'a11y/selector-pseudo-class-focus': true,
+
+    'color-format/format': {
+      format: 'rgb',
+    },
+  },
+};
