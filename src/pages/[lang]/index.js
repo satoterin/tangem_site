91caf4d1ab2916fa3768f5c69bee44fe49280c1b
satoterin <judekinsley911@gmail.com>
2022-07-14 17:12:00 +0800

    JS-33 Add Yandex Metrika

--- src/pages/[lang]/index.js
+++ src/pages/[lang]/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Header from '../../components/Common/Header'
 import SectionHero from '../../components/Home/SectionHero'
 import SectionFeature from '../../components/Home/SectionFeature'
@@ -12,33 +11,17 @@ import ModalNew from "../../components/Common/Modal";
 import Search from "../../components/Home/Search";
 import useModal from "../../hooks/useModal";
 import Pricing from "../../components/Common/Pricing";
-import i18next, {t} from 'i18next';
+import {t} from 'i18next';
 import { getAllLanguageSlugs, getLanguage } from '../../lib/lang';
 import Footer from "../../components/Common/Footer";
+import Layout from "../../components/Common/Layout";
 
 export const LangHome = ({ language }) => {
 	const { isShowing: isSearchShowing, toggle: toggleSearch } = useModal('search')
 	const { isShowing: isBuyShowing, toggle: toggleBuy } = useModal('pricing')
 
 	return (
-		<>
-			<Head>
-				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<meta name="title" content={ i18next.t('title') } />
-				<meta name="description" content={ i18next.t('description') } />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://tangem.com" />
-				<meta property="og:site_name" content={ i18next.t('title') } />
-				<meta property="og:title" content={ i18next.t('title') } />
-				<meta property="og:description" content={ i18next.t('description') } />
-				<meta property="og:image" content="https://tangem.com/img/hero/phone@1x.png" />
-				<meta property="og:video" content="https://www.youtube.com/watch?v=ST4jvcaE_UU" />
-				<meta property="og:locale" content="en_US" />
-				<title>{ t('title')}</title>
-				<link rel='shortcut icon' href='/img/favicon/favicon.png' />
-				<link rel='apple-touch-icon' href='/img/favicon/favicon-180.png' />
-			</Head>
+		<Layout title={t('title')} description={t('description') }>
 			<Header isDark={false} toggleBuy={toggleBuy}/>
 			<main>
 				<SectionHero
@@ -68,7 +51,7 @@ export const LangHome = ({ language }) => {
 				</ModalNew>
 			</main>
 			<Footer />
-		</>
+		</Layout>
 	)
 }
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import i18next, {t} from "i18next";
+import i18next from "i18next";
 
 export default function Document() {
 	const { language } = i18next;
@@ -7,27 +7,8 @@ export default function Document() {
   return (
     <Html lang={language}>
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-N7NQVMC');
-            `,
-          }}
-        />
       </Head>
       <body>
-        <noscript>
-	        <iframe
-		        src="https://www.googletagmanager.com/ns.html?id=GTM-N7NQVMC"
-						height="0"
-		        width="0"
-		        style={{ display: "none", visibility: "hidden"}}
-	        />
-				</noscript>
         <Main />
         <div id="portal" />
         <NextScript />
